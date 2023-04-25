f4f056ad71bfca3deede02b6fb142b3e033de4e5
Sukanth07 <sukanthkannansk@gmail.com>
2023-04-25 19:19:49 +0530

    reset

--- overlay/urls.py
+++ overlay/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("overlay/", views.home, name="home")
+    path("overlay/", views.overlay_jewellery, name="overlay_jewellery")
 ]
